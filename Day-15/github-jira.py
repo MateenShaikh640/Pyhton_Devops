9dc9c5750869f6db15ad35e3c0ebd3e50a973ce6
MateenShaikh640 <154122936+MateenShaikh640@users.noreply.github.com>
2023-12-28 09:04:18 +0530

    Update github-jira.py

--- Day-15/github-jira.py
+++ Day-15/github-jira.py
@@ -15,7 +15,7 @@ def createJira():
 
     API_TOKEN=""
 
-    auth = HTTPBasicAuth("", API_TOKEN)
+    auth = HTTPBasicAuth("mateenshaikh046@gmail.com", API_TOKEN)
 
     headers = {
         "Accept": "application/json",
